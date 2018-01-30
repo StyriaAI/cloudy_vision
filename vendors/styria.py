317d9a93c066eecaa1781e53eda06cfec80229d9
Vjekoslav Giacometti <vjekoslav.giacometti@styria.hr>
2018-01-30 16:08:22 +0100

    Styria vendor improved to work with categorization api.

--- vendors/styria.py
+++ vendors/styria.py
@@ -1,7 +1,18 @@
 # -*- coding: utf-8 -*-
 import requests
+from io import BytesIO
 
-STYRIA_URL = 'https://ds-cloud.styria.hr/categorization/public/api/test/m0/categorize'  # noqa
+from PIL import Image
+
+STYRIA_URL = 'https://ds-cloud.styria.hr/categorization/api/general/categorize'  # noqa
+
+
+def _load_and_resize_image(image_path, out_longer_side=256):
+    out_img = BytesIO()
+    image = Image.open(image_path)
+    image.thumbnail((out_longer_side, out_longer_side))
+    image.save(out_img, format="JPEG", quality=70)
+    return out_img.getvalue()
 
 
 def call_vision_api(image_filename, api_keys):
@@ -10,9 +21,10 @@ def call_vision_api(image_filename, api_keys):
     # image_filename = os.path.abspath(image_filename)
 
     response = requests.post(
+        STYRIA_URL,
         headers={"Authorization": api_key},
         params={"verbose": 1},
-        files={"image1": open(image_filename, "rb")})
+        files={"image1": _load_and_resize_image(image_filename)})
 
     if response.status_code != 200:
         raise Exception("Error: {}".format(response.text))
